e535c1f9da424dcdb03c87822cf82531c1c91188
Evgenii Mozharovskii <mozharovsky@live.com>
2026-08-06 16:55:29 +0100

    chore(release): prepare 0.9.31

--- Sources/RorkDevice/Public/RorkDevice.swift
+++ Sources/RorkDevice/Public/RorkDevice.swift
@@ -7,7 +7,7 @@ import Foundation
 /// workflows.
 public enum RorkDevice {
     /// Package version reported by APIs and command-line diagnostics.
-    public static let version = "0.9.30"
+    public static let version = "0.9.31"
 }
 
 /// Returns whether usbmux identifies a discovered device record as a USB route.
